52bd8576e96ee69f74e7c3055901ef85d4d160cb
cdle <798731886@qq.com>
2021-09-17 07:16:11 +0800

    update

--- core/init.go
+++ core/init.go
@@ -3,13 +3,14 @@ package core
 import "os"
 
 func init() {
-	initStore()
 	killp()
 	for _, arg := range os.Args {
 		if arg == "-d" {
+			initStore()
 			Daemon()
 		}
 	}
+	initStore()
 	ReadYaml(ExecPath+"/conf/", &Config, "https://raw.githubusercontent.com/cdle/sillyGirl/main/conf/demo_config.yaml")
 	InitReplies()
 	initToHandleMessage()
