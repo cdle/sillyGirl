6437ff4d35a5f3b4944567be8ec4f1567192ec98
cdle <xiaocandle@gmail.com>
2021-09-26 17:44:42 +0800

    feat(core): adjust store initialization order

--- core/init.go
+++ core/init.go
@@ -10,6 +10,13 @@ import (
 var Duration time.Duration
 
 func init() {
+	killp()
+	for _, arg := range os.Args {
+		if arg == "-d" {
+			initStore()
+			Daemon()
+		}
+	}
 	_, err := os.Stat("/etc/sillyGirl/")
 	if err != nil {
 		os.MkdirAll("/etc/sillyGirl/", os.ModePerm)
@@ -24,7 +31,10 @@ func init() {
 		for scanner.Scan() {
 			line := scanner.Text()
 			if v := regexp.MustCompile(`^\s*set\s+(\S+)\s+(\S+)\s+(\S+)`).FindStringSubmatch(line); len(v) > 0 {
-				Bucket(v[1]).Set(v[2], v[3])
+				b := Bucket(v[1])
+				if b.Get(v[2]) != v[3] {
+					b.Set(v[2], v[3])
+				}
 			}
 		}
 		file.Close()
