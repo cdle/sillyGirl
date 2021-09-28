d0ecb72574168ba1c03cc838da1e2ab1177c5780
cdle <xiaocandle@gmail.com>
2021-09-28 18:14:51 +0800

    feat(core): add init

---
+++ core/env.go
@@ -0,0 +1,19 @@
+package core
+
+import "fmt"
+
+var env = NewBucket("env")
+
+func init() {
+	// var xa = `https://jintia.jintias.cn/api/xatx.php?msg={{1}}`
+	// replies := []string{
+	// 	`你是，(.*)？=>你好，我是{{1}}。`,
+	// 	`(小爱\S*)=>gjson(req(xa,1), text)`,
+	// }
+	// fmt.Println(replies)
+	var template = `
+var content = {{1}}
+
+	`
+	fmt.Println(template)
+}
