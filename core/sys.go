3c652a85510a970c9eedc14d68be0c0c29d83870
cdle <798731886@qq.com>
2021-09-30 20:43:47 +0800

    update

--- core/sys.go
+++ core/sys.go
@@ -60,7 +60,9 @@ func GitPull(filename string) (bool, error) {
 }
 
 func CompileCode() error {
-	_, err := exec.Command("sh", "-c", "cd "+ExecPath+" && go build -o "+pname).Output()
+	cmd := exec.Command("go", "build")
+	cmd.Path = ExecPath
+	_, err := cmd.Output()
 	if err != nil {
 		return errors.New("编译失败：" + err.Error() + "。")
 	}
