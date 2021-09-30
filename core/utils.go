61fc29b56790e963c665e01e17cf7ad6578d69af
cdle <xiaocandle@gmail.com>
2021-09-30 21:09:34 +0800

    feat(core): add WriteToFile

---
+++ core/utils.go
@@ -0,0 +1,18 @@
+package core
+
+import (
+	"fmt"
+	"os"
+)
+
+func WriteToFile(fileName string, content string) error {
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("file create failed. err: " + err.Error())
+	} else {
+		n, _ := f.Seek(0, os.SEEK_END)
+		_, err = f.WriteAt([]byte(content), n)
+		defer f.Close()
+	}
+	return err
+}
