9c84dda9800c24ad5ed84c9d7268b218456a6b8f
cdle <xiaocandle@gmail.com>
2021-10-04 20:28:05 +0800

    feat(core): adjust push, test

--- core/test.go
+++ core/test.go
@@ -17,9 +17,9 @@ func init() {
 		if v != "" {
 			vv := strings.Split(v, " ")
 			tp, cd, ud := vv[0], Int(vv[1]), Int(vv[2])
-			if tp == "fake" && sillyGirl.GetBool("update_notify", false) == true { //
-				time.Sleep(time.Second * 10)
-				NotifyMasters("自动更新完成。")
+			if tp == "fake" { //&& sillyGirl.GetBool("update_notify", false) == true { //
+				// time.Sleep(time.Second * 10)
+				// NotifyMasters("自动更新完成。")
 				return
 			}
 			msg := "重启完成。"
