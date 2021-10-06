e4c27ed95b1e27ad21a2560c03d4798c422f36ee
cdle <798731886@qq.com>
2021-10-07 07:57:28 +0800

    update

--- im/tg/tg.go
+++ im/tg/tg.go
@@ -97,7 +97,7 @@ func init() {
 			b.Send(ct, s)
 		}
 		b.Handle(tb.OnPhoto, func(m *tb.Message) {
-
+			b.Send(m.Chat, m.Caption)
 			b.SendAlbum(m.Chat, tb.Album{&tb.Photo{File: tb.FromReader(m.Photo.FileReader)}})
 
 			// b.Download(&m.Photo.File,"")
