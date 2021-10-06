c6790305ccb20831321c10d801ac11ec4502a88c
cdle <798731886@qq.com>
2021-10-07 04:22:20 +0800

    update

--- im/tg/tg.go
+++ im/tg/tg.go
@@ -97,7 +97,8 @@ func init() {
 			b.Send(ct, s)
 		}
 		b.Handle(tb.OnPhoto, func(m *tb.Message) {
-			m.Text = fmt.Sprintf(`[CQ:image,url=%s]`, m.Photo.FileURL) + m.Caption + m.Photo.FileURL
+			m.Text = fmt.Sprintf(`[CQ:image,url=%s]`, m.Photo.FileURL) + m.Caption
+			core.NotifyMasters(fmt.Sprintf(`[CQ:image,url=%s]`, m.Photo.FileURL) + m.Caption)
 			Handler(m)
 		})
 		b.Handle(tb.OnText, Handler)
