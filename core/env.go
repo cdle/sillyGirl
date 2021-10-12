9e3a13a352c700add5938b10c90496df6b46c43d
cdle <xiaocandle@gmail.com>
2021-10-12 09:41:18 +0800

    refactor(core): adjust env

--- core/env.go
+++ core/env.go
@@ -1,20 +1,17 @@
 package core
 
-import "fmt"
-
 var env = NewBucket("env")
 
-func init() {
-	// var xa = `https://jintia.jintias.cn/api/xatx.php?msg={{1}}`
-	// replies := []string{
-	// 	`你是，(.*)？=>你好，我是{{1}}。`,
-	// 	`(小爱\S*)=>gjson(req(xa,1), text)`,
-	// }
-	// fmt.Println(replies)
-	var template = `
-var content = {{1}}
-var data = request
+// func init() {
+// 	// var xa = `https://jintia.jintias.cn/api/xatx.php?msg={{1}}`
+// 	// replies := []string{
+// 	// 	`你是，(.*)？=>你好，我是{{1}}。`,
+// 	// 	`(小爱\S*)=>gjson(req(xa,1), text)`,
+// 	// }
+// 	// fmt.Println(replies)
+// 	var template = `
+// var content = {{1}}
+// var data = request
 
-	`
-	fmt.Println(template)
-}
+// 	`
+// }
