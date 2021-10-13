cc6ec8f48528bbd27243469ac2c3d44940e6b90f
cdle <50124752+cdle@users.noreply.github.com>
2021-10-14 05:42:45 +0800

    Update tg.go

--- im/tg/tg.go
+++ im/tg/tg.go
@@ -64,6 +64,7 @@ func init() {
 			Token:  token,
 			Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 			// ParseMode: tb.ModeMarkdownV2,
+                        Url: tg.Get("url"),
                         
 		}
 		if url := tg.Get("http_proxy"); url != "" {
