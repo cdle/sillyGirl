904bdc20c80faa415feeedd8c18c8ec0742458f0
cdle <798731886@qq.com>
2021-10-20 21:48:47 +0800

    update

---
+++ core/recall.go
@@ -0,0 +1,24 @@
+package core
+
+import "strings"
+
+func init() {
+	go func() {
+		recall := sillyGirl.Get("recall")
+		if recall != "" {
+			rules := []string{}
+			for _, v := range strings.Split(recall, "&") {
+				rules = append(rules, "raw "+v)
+			}
+			AddCommand("", []Function{
+				{
+					Rules: rules,
+					Handle: func(s Sender) interface{} {
+						s.Delete()
+						return nil
+					},
+				},
+			})
+		}
+	}()
+}
