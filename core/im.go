0f4204e9353b3839ffd72ef9c0b89734521293b5
cdle <798731886@qq.com>
2021-11-05 08:47:56 +0800

    update

--- core/im.go
+++ core/im.go
@@ -255,18 +255,18 @@ var ForGroup forGroup
 
 func (_ *BaseSender) Await(sender Sender, callback func(Sender) interface{}, params ...interface{}) interface{} {
 	c := &Carry{}
-	// timeout := time.Second * 86400000
+	timeout := time.Second * 86400000
 	var handleErr func(error)
 	var fg *forGroup
 	for _, param := range params {
 		switch param.(type) {
 		case string:
 			c.Pattern = param.(string)
-		// case time.Duration:
-		// 	du := param.(time.Duration)
-		// 	if du != 0 {
-		// timeout = du
-		// }
+		case time.Duration:
+			du := param.(time.Duration)
+			if du != 0 {
+				timeout = du
+			}
 		case func() string:
 			callback = param.(func(Sender) interface{})
 		case func(error):
@@ -314,6 +314,7 @@ func (_ *BaseSender) Await(sender Sender, callback func(Sender) interface{}, par
 					}
 					c.Result <- "Y or n ?"
 				} else {
+					c.Result <- result
 					return nil
 				}
 			case error:
@@ -323,12 +324,12 @@ func (_ *BaseSender) Await(sender Sender, callback func(Sender) interface{}, par
 				c.Result <- nil
 				return nil
 			}
-			// case <-time.After(timeout):
-			// 	if handleErr != nil {
-			// 		handleErr(TimeOutError)
-			// 	}
-			// 	c.Result <- nil
-			// 	return nil
+		case <-time.After(timeout):
+			if handleErr != nil {
+				handleErr(TimeOutError)
+			}
+			c.Result <- nil
+			return nil
 		}
 	}
 }
