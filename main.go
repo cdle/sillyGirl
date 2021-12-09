3ca56aa1a502eb6263aa54120e0ad00e86c5f9ae
cdle <798731886@qq.com>
2021-12-09 21:54:02 +0800

    update

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/cdle/sillyGirl/core"
+import (
+	"github.com/beego/beego/v2/core/logs"
+	"github.com/cdle/sillyGirl/core"
+)
 
 func main() {
 	go core.RunServer()
+	logs.Info("傻妞用不了了？关注频道 https://t.me/kczz2021 获取最新消息。")
 	select {}
+
 }
