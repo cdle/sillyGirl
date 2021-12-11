9599a39155bae52291464a395472ce8fb1ae530a
cdle <798731886@qq.com>
2021-12-11 21:33:06 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/beego/beego/v2/core/logs"
 	"github.com/cdle/sillyGirl/core"
@@ -11,13 +13,12 @@ func main() {
 	go core.RunServer()
 	logs.Info("傻妞用不了了？关注频道 https://t.me/kczz2021 获取最新消息。")
 	fmt.Println("你可以在这里输入命令：")
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		var input string
-		fmt.Scanln(&input)
-		fmt.Println(input)
+		data, _, _ := reader.ReadLine()
 		core.Senders <- &core.Faker{
 			Type:    "terminal",
-			Message: input,
+			Message: data,
 		}
 	}
 }
