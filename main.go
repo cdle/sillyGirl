959e2ad8d7b3fc354c12526060f24e6bc8ed7602
cdle <798731886@qq.com>
2021-12-11 21:16:42 +0800

    update

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	go core.RunServer()
 	logs.Info("傻妞用不了了？关注频道 https://t.me/kczz2021 获取最新消息。")
+	fmt.Println("你可以在这里输入命令：")
 	for {
 		var input string
 		fmt.Scanln(&input)
