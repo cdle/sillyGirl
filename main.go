555e1c0e44bfc2edc5346b30c99ee111244c5a8e
cdle <798731886@qq.com>
2021-12-11 22:56:02 +0800

    update

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"os"
-
 	"github.com/beego/beego/v2/core/logs"
 	"github.com/cdle/sillyGirl/core"
 )
@@ -15,12 +12,13 @@ func main() {
 	logs.Info("Http服务已运行(%s)。", sillyGirl.Get("port", "8080"))
 	go core.Server.Run("0.0.0.0:" + port)
 	logs.Info("关注频道 https://t.me/kczz2021 获取最新消息。")
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		data, _, _ := reader.ReadLine()
-		core.Senders <- &core.Faker{
-			Type:    "terminal",
-			Message: string(data),
-		}
-	}
+	// reader := bufio.NewReader(os.Stdin)
+	// for {
+	// 	data, _, _ := reader.ReadLine()
+	// 	core.Senders <- &core.Faker{
+	// 		Type:    "terminal",
+	// 		Message: string(data),
+	// 	}
+	// }
+	select {}
 }
