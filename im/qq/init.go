0889f585de7bcc88a9f460f21a551daf8e4309cd
cdle <798731886@qq.com>
2021-12-30 13:18:38 +0800

    update

--- im/qq/init.go
+++ im/qq/init.go
@@ -190,7 +190,7 @@ func init() {
 		go func() {
 			for {
 				_, data, err := ws.ReadMessage()
-				fmt.Println(string(data))
+				// fmt.Println(string(data))
 				if err != nil {
 					ws.Close()
 					logs.Info("QQ机器人(%s)已断开。", botID)
