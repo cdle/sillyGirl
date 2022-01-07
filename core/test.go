bfddd96285ce3be882f7685e992d4c547b6e20ec
cdle <798731886@qq.com>
2022-01-07 21:08:38 +0800

    update

--- core/test.go
+++ core/test.go
@@ -540,7 +540,7 @@ Alias=sillyGirl.service`
 					begin = strings.Replace(regexp.MustCompile(`([一-龥])】`).FindString(cy), "】", "", -1)
 					return begin
 				}
-				id := fmt.Sprintf("%v", time.Now().UnixMicro())
+				id := fmt.Sprintf("%v", time.Now().Unix())
 			start:
 				data, err := httplib.Get("http://hm.suol.cc/API/cyjl.php?id=" + id + "&msg=开始成语接龙").String()
 				if err != nil {
