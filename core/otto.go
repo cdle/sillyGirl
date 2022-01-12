7b135bf9f459331e8fede94edc7f878dc5ca2227
cdle <798731886@qq.com>
2022-01-12 13:47:41 +0800

    update

--- core/otto.go
+++ core/otto.go
@@ -54,6 +54,18 @@ func (sender *Fmt) Sprintf(format string, a ...interface{}) string {
 	return fmt.Sprintf(format, a...)
 }
 
+func (sender *Fmt) Printf(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
+}
+
+func (sender *Fmt) Println(a ...interface{}) (int, error) {
+	return fmt.Println(a...)
+}
+
+func (sender *Fmt) Print(a ...interface{}) (int, error) {
+	return fmt.Print(a...)
+}
+
 type JsSender struct {
 	Sender Sender
 	again  string
