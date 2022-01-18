9712147a643f8f1d0fd6f691a0a5ca8111e8543c
cdle <798731886@qq.com>
2022-01-18 12:39:18 +0800

    update

--- core/push.go
+++ core/push.go
@@ -2,8 +2,6 @@ package core
 
 import (
 	"strings"
-
-	"github.com/beego/beego/v2/adapter/httplib"
 )
 
 var Pushs = map[string]func(interface{}, string, interface{}, string){}
@@ -34,9 +32,9 @@ func NotifyMasters(content string) {
 		if sillyGirl.GetBool("ignore_notify", false) == true {
 			return
 		}
-		if token := sillyGirl.Get("pushplus"); token != "" {
-			httplib.Get("http://www.pushplus.plus/send?token=" + token + "&title=0101010&content=" + content + "&template=html")
-		}
+		// if token := sillyGirl.Get("pushplus"); token != "" {
+		// 	httplib.Get("http://www.pushplus.plus/send?token=" + token + "&title=0101010&content=" + content + "&template=html")
+		// }
 		for _, class := range []string{"tg", "qq", "wx"} {
 			notify := Bucket(class).Get("notifiers")
 			if notify == "" {
