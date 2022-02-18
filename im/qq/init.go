459b25dafcef62fae3c1b16591ab447840efc942
mzzsfy <mzzsfy@163.com>
2022-02-19 02:03:51 +0800

    test

--- im/qq/init.go
+++ im/qq/init.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/beego/beego/v2/adapter/logs"
 	"github.com/cdle/sillyGirl/core"
+	_ "github.com/cdle/sillyGirl/develop/boltdb"
 	"github.com/cdle/sillyGirl/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
