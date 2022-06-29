aaf783f009f24e13e2498279004c117822098874
mzzsfy <mzzsfy@163.com>
2022-06-29 17:23:01 +0800

    test

--- core/function.go
+++ core/function.go
@@ -156,6 +156,11 @@ func HandleMessage(sender Sender) {
 		}
 		if m := regexp.MustCompile(c.Pattern).FindString(content); m != "" {
 			mtd = true
+			if f, ok := c.Sender.(*Faker); ok && f.Carry != nil {
+				if s1, o := sender.(*Faker); o && s1.Carry != nil {
+					f.Carry = s1.Carry
+				}
+			}
 			c.Chan <- sender
 			sender.Reply(<-c.Result)
 			if !sender.IsContinue() {
