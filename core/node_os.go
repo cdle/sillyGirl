74facf02eac02f0122cb38ce06697b36aea5704f
cdle <xiaocandle@gmail.com>
2023-06-08 09:15:35 +0800

    feat(deps): adjust node_os, go.mod, go.sum

--- core/node_os.go
+++ core/node_os.go
@@ -2,10 +2,12 @@ package core
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
 
+	iconv "github.com/djimenez/iconv-go"
 	"github.com/dop251/goja"
 )
 
@@ -19,14 +21,29 @@ func getJsOs(vm *goja.Runtime, running func() bool) *goja.Object {
 		return data
 	})
 	jsos.Set("readFileSync", func(path string) string {
-		data, err := os.ReadFile(path)
+		content, err := ioutil.ReadFile(path)
 		if err != nil {
 			panic(Error(vm, err))
 		}
-		return string(data)
+		// 自动转换编码为UTF-8
+		utf8Content, err := iconv.ConvertString(string(content), "auto", "utf-8")
+		if err != nil {
+			panic(Error(vm, err))
+		}
+		return utf8Content
 	})
-	jsos.Set("writeFileSync", func(path, content string) {
-		err := os.WriteFile(path, []byte(content), 0644)
+
+	jsos.Set("writeFileSync", func(path, content string, encode string) {
+		// 将文本内容转换为GBK编码
+		var err error
+		if !(encode == "" || encode == "utf-8") {
+			content, err = iconv.ConvertString(content, "utf-8", encode)
+			if err != nil {
+				panic(Error(vm, err))
+			}
+		}
+		// 将文本内容以GBK编码写入文件
+		err = ioutil.WriteFile(path, []byte(content), 0644)
 		if err != nil {
 			panic(Error(vm, err))
 		}
--- go.mod
+++ go.mod
@@ -11,16 +11,17 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/buger/jsonparser v1.1.1
 	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
 	github.com/dop251/goja v0.0.0-20230427124612-428fc442ff5f
 	github.com/dop251/goja_nodejs v0.0.0-20230322100729-2550c7b6c124
 	github.com/elastic/go-elasticsearch/v6 v6.8.5
-	github.com/forPelevin/gomoji v1.1.8
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/goccy/go-json v0.9.7
 	github.com/gogo/protobuf v1.3.1
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/websocket v1.5.0
+	github.com/rivo/uniseg v0.4.3
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/stretchr/testify v1.8.0
 	golang.org/x/net v0.4.0
@@ -31,7 +32,6 @@ require (
 	github.com/google/pprof v0.0.0-20230510103437-eeec1cb781c3 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
