91c7c2e0a5ac64794cd4bdefc68a88f6d34cbe09
cdle <xiaocandle@gmail.com>
2023-06-08 12:14:45 +0800

    feat(deps): add ConvertToString

--- core/node_os.go
+++ core/node_os.go
@@ -1,18 +1,14 @@
 package core
 
 import (
-	"bytes"
 	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 
+	"github.com/axgle/mahonia"
 	"github.com/dop251/goja"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding/ianaindex"
-	"golang.org/x/text/transform"
 )
 
 func getJsOs(vm *goja.Runtime, running func() bool) *goja.Object {
@@ -24,48 +20,80 @@ func getJsOs(vm *goja.Runtime, running func() bool) *goja.Object {
 		}
 		return data
 	})
-	jsos.Set("readFileSync", func(path string) string {
+
+	jsos.Set("readFileSync", func(path string, decode string) string {
 		// 读取文件内容
 		content, err := ioutil.ReadFile(path)
 		if err != nil {
 			panic(Error(vm, err))
 		}
-
-		// 从文件内容中自动检测编码
-		encoding, _, _ := charset.DetermineEncoding(content, "")
-
-		// 创建一个将编码转换为 UTF-8 的转换器
-		utf8Reader := transform.NewReader(bytes.NewReader(content), encoding.NewDecoder())
-
-		// 读取转换后的 UTF-8 数据
-		utf8Content, err := ioutil.ReadAll(utf8Reader)
-		if err != nil {
-			panic(Error(vm, err))
+		if decode == "" || decode == "utf-8" {
+			return string(content)
 		}
-		return string(utf8Content)
+		srcCoder := mahonia.NewDecoder(decode)
+		srcResult := srcCoder.ConvertString(string(content))
+		return srcResult
+		// return ConvertToString(string(content), decode, "utf-8")
+		// if decode == "" {
+		// 	return string(content)
+		// }
+		// coder := mahonia.NewEncoder(decode)
+		// return coder.ConvertString(string(content))
+
+		// // 从文件内容中自动检测编码
+		// encoding, name, _ := charset.DetermineEncoding(content, "")
+		// logs.Debug("name", name)
+
+		// // 创建一个将编码转换为 UTF-8 的转换器
+		// utf8Reader := transform.NewReader(bytes.NewReader(content), encoding.NewDecoder())
+
+		// // 读取转换后的 UTF-8 数据
+		// utf8Content, err := ioutil.ReadAll(utf8Reader)
+		// if err != nil {
+		// 	panic(Error(vm, err))
+		// }
+		// return string(utf8Content)
 	})
 
 	jsos.Set("writeFileSync", func(path, content string, encode string) {
-
-		encode = strings.ToLower(encode)
-		if encode == "" {
-			encode = "utf-8"
-		}
-		// 将UTF-8编码的文本转换为指定编码
-		index := ianaindex.MIME
-		enc, err := index.Encoding(encode)
-		if err != nil {
-			panic(Error(vm, err))
-		}
-		converted, _, err := transform.String(enc.NewEncoder(), content)
-		if err != nil {
-			panic(Error(vm, err))
+		converted := ""
+		if encode == "" || encode == "utf-8" {
+			converted = content
+		} else {
+			tagCoder := mahonia.NewEncoder(encode)
+			converted = tagCoder.ConvertString(content)
 		}
 		// 覆盖写入文件
-		err = os.WriteFile(path, []byte(converted), 0644)
+		err := os.WriteFile(path, []byte(converted), 0644)
 		if err != nil {
 			panic(Error(vm, err))
 		}
+		// if encode == "" {
+		// 	encode = "utf-8"
+		// }
+		// if encode == "utf-8" {
+		// 	// 覆盖写入文件
+		// 	err := os.WriteFile(path, []byte(content), 0644)
+		// 	if err != nil {
+		// 		panic(Error(vm, err))
+		// 	}
+		// 	return
+		// }
+		// // 将UTF-8编码的文本转换为指定编码
+		// index := ianaindex.MIME
+		// enc, err := index.Encoding(encode)
+		// if err != nil {
+		// 	panic(Error(vm, err))
+		// }
+		// converted, _, err := transform.String(enc.NewEncoder(), content)
+		// if err != nil {
+		// 	panic(Error(vm, err))
+		// }
+		// // 覆盖写入文件
+		// err = os.WriteFile(path, []byte(converted), 0644)
+		// if err != nil {
+		// 	panic(Error(vm, err))
+		// }
 	})
 	jsos.Set("walkFilePath", func(root string, callback func(path string, info os.FileInfo) bool) {
 		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -94,4 +122,23 @@ func getJsOs(vm *goja.Runtime, running func() bool) *goja.Object {
 	jsos.Set("name", runtime.GOOS)
 	jsos.Set("arch", runtime.GOARCH)
 	return jsos
+}
+
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	if srcCode == tagCode {
+		return src
+	}
+	srcResult := ""
+	if srcCode == "utf-8" {
+		srcResult = src
+	} else {
+		srcCoder := mahonia.NewDecoder(srcCode)
+		srcResult = srcCoder.ConvertString(src)
+	}
+	if tagCode == "utf-8" {
+		return srcResult
+	}
+	tagCoder := mahonia.NewEncoder(tagCode)
+	result := tagCoder.ConvertString(srcResult)
+	return result
 }
--- go.mod
+++ go.mod
@@ -7,11 +7,11 @@ replace github.com/willf/bitset v1.2.0 => github.com/bits-and-blooms/bitset v1.2
 replace gorm.io/gorm => github.com/go-gorm/gorm v1.22.5
 
 require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
 	github.com/beego/beego/v2 v2.1.0
 	github.com/boltdb/bolt v1.3.1
 	github.com/buger/jsonparser v1.1.1
 	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58
-	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
 	github.com/dop251/goja v0.0.0-20230605162241-28ee0ee714f3
 	github.com/dop251/goja_nodejs v0.0.0-20230602164024-804a84515562
 	github.com/elastic/go-elasticsearch/v6 v6.8.10
@@ -38,7 +38,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
@@ -62,6 +61,6 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/text v0.9.0
 	google.golang.org/protobuf v1.30.0 // indirect
 )
